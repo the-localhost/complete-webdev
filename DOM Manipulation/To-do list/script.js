447de13f8f584670415ed6e23fae3d05c5178a8b
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-05-03 19:51:16 +0530

    generalized fn created to avoid code repitition

--- DOM Manipulation/To-do list/script.js
+++ DOM Manipulation/To-do list/script.js
@@ -6,21 +6,22 @@ function inputLength() {
 	return input.value.length;
 }
 
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
 function addListAfterClick() {
 	if (inputLength() > 0) {
-		var li = document.createElement("li");
-		li.appendChild(document.createTextNode(input.value));
-		ul.appendChild(li);
-		input.value = "";
+		createListElement();
 	}
 }
 
 function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
-		var li = document.createElement("li");
-		li.appendChild(document.createTextNode(input.value));
-		ul.appendChild(li);
-		input.value = "";
+		createListElement();
 	}
 }
 
