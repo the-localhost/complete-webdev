909b72a39dbb6556d75770f160d131abc22ac27c
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-05-04 18:45:01 +0530

    added the onchange attribute to color inputs

--- DOM Manipulation/Gradient Generator/script.js
+++ DOM Manipulation/Gradient Generator/script.js
@@ -1,3 +1,12 @@
-var color1 = document.getElementsByName("color1")[0];
-var color2 = document.getElementsByName("color1")[1];
+var colorSelector1 = document.getElementsByName("color1")[0];
+var colorSelector2 = document.getElementsByName("color1")[1];
 var body = document.querySelector("body");
+
+// colorSelector1.addEventListener("onchange", );
+
+function change_color(){
+    var color1 = colorSelector1.value;
+    var color2 = colorSelector2.value;
+    var bg = "linear-gradient(to right, "+color1+", "+color2+")";
+    body.style.background = bg;
+}
