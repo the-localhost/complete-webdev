84f5325df7ec2bb3e45c2f08be20fd4bd654733e
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-05-09 12:42:40 +0530

    updated the problem statement

---
+++ DOM Manipulation/Gradient Generator/Advanced Javascript/Coding Challenge/question3.js
@@ -0,0 +1,5 @@
+/**
+ * Write a function that converts HEX to RGB. Then Make that function auto-detect
+ * the formats so that if you enter HEX color format it returns RGB and if you 
+ * enter RGB color format it returns HEX
+*/
