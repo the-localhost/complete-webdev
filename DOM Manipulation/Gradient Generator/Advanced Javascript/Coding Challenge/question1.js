f2adac96123f61779eec20c64ea853eae2ff17fa
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-05-09 12:34:57 +0530

    added other method without code with TC and SC

--- DOM Manipulation/Gradient Generator/Advanced Javascript/Coding Challenge/question1.js
+++ DOM Manipulation/Gradient Generator/Advanced Javascript/Coding Challenge/question1.js
@@ -4,8 +4,6 @@
  * For example answer(ArrayFromAbove) should return: [[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591].
 */
 
-const arr = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
-
 function freqArr(elem, freq) {       // returns an array of length freq with all elements as elem
     const res = [];
     for(let i=0; i<freq; i++) {
@@ -30,8 +28,16 @@ function cleanIt(arr) {
     return finals;
 }
 
+const arr = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
 const cleanArr = cleanIt(arr);
 console.log(JSON.stringify(cleanArr));
 
 // Time Complexity: O(2n) = O(n)
 // Space complexity: O(N), N = number of unique elements
+
+/** Method2: We sort the array first, and then traverse it and make groups.
+ *  Time Complexity: O(nlogn) + O(n) = O(nlogn)
+ *  Space Complexity: O(n)
+ *          constant extra space if we can modify the original array
+ *          O(n) extra space otherwise
+*/
