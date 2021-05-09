4dd4fba3199d0816bea5eeb6d1b47b32fcf91f01
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-05-09 12:25:34 +0530

    solved the problem

--- DOM Manipulation/Gradient Generator/Advanced Javascript/Coding Challenge/question2.js
+++ DOM Manipulation/Gradient Generator/Advanced Javascript/Coding Challenge/question2.js
@@ -3,3 +3,20 @@
  * The function should find two different numbers in the array that, when added together, 
  * give the target number. For example: answer([1,2,3], 4)should return [1,3]
 */
+function findTwoNumbersSum(arr, sum){
+    arr.sort((a,b) => a-b);
+    for(let i=0; i<arr.length-1; i++){
+        let elem = arr[i];
+        if(elem>=sum){
+            return "Numbers don't exist";
+        }
+        let leftSum = sum-elem;
+        for(let j=i+1; j<arr.length; j++){
+            if(arr[j]>leftSum) break;
+            if(arr[j]==leftSum){
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
+console.log(findTwoNumbersSum([1,2,3], 4))
