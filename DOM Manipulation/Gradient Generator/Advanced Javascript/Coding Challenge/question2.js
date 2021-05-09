91440468b3afe841be7d623aecce127f0db7fe81
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-05-09 12:24:05 +0530

    problem is written

---
+++ DOM Manipulation/Gradient Generator/Advanced Javascript/Coding Challenge/question2.js
@@ -0,0 +1,5 @@
+/**
+ * Write a javascript function that takes an array of numbers and a target number. 
+ * The function should find two different numbers in the array that, when added together, 
+ * give the target number. For example: answer([1,2,3], 4)should return [1,3]
+*/
