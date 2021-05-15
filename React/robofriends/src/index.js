5fa039af0f4aa430677b14c3e016c0251c4f2d5b
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-05-15 20:22:45 +0530

    impoted tachyons

--- React/robofriends/src/index.js
+++ React/robofriends/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 //import App from './App';
 import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
