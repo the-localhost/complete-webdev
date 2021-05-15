f205c39c7c857a8a2033e3a2dc2c857e05771684
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-05-15 20:43:19 +0530

    achieveing same component functionality using a function

--- React/robofriends/src/Hello.js
+++ React/robofriends/src/Hello.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import './Hello.css';
 
+/*
 class Hello extends Component {
     render() {
-        /** here we use classNames in div attributes as this is JSX and not HTML
-         *  'class' is already a keyword in JS, so it will give warning but will work
-        */
+        // here we use classNames in div attributes as this is JSX and not HTML
+        // 'class' is already a keyword in JS, so it will give warning but will work
         return (
             <div className="f1 tc">
                 <h1>Hey, hello!</h1>
@@ -16,5 +16,15 @@ class Hello extends Component {
         )
     }
 }
-
+*/
+const Hello = (props) => {
+    return (
+        <div className="f1 tc">
+            <h1>Hey, hello!</h1>
+            <p>Read our <a href="#">guide to work</a>, if this is your first day.</p>
+            <p>This is what we got:</p><br/>
+            <p>{props.greeting}</p>
+        </div>
+    )
+}
 export default Hello;
